0bcc6c5cbaecfb2d35f201f83203749d8b684e1b
jacky cheung <2541012495@qq.com>
2018-12-18 22:29:44 +0800

    新增智能组卷界面，以及部分插件，css，img文件等

---
+++ js/combination.js
@@ -0,0 +1,7 @@
+(function(){
+    layui.use(['element','laypage','form'], function(){
+        var element = layui.element,
+            laypage = layui.laypage;
+            var form = layui.form;
+        })
+})()
